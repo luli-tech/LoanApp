6abac0815dc90e216f0d712583eae655323f6804
luli-tech <olabodemicheal5@gmail.com>
2024-12-02 14:35:12 +0100

    error profile

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./userprofile.css";
+import "./userProfile.css";
 import { useSelector, useDispatch } from "react-redux";
 import { setProfile } from "./store";
 
