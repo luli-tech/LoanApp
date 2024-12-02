66a5223fed73b82097d0684d840c579941c8520b
luli-tech <olabodemicheal5@gmail.com>
2024-12-02 14:30:58 +0100

    approve

--- src/components/approve.jsx
+++ src/components/approve.jsx
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
 import SuccessMessage from "./successmessage";
-import "./Successmessage.css";
 import { useNavigate } from "react-router-dom";
 
 const LoanApplication = () => {
-  let navigate = useNavigate();
-  const [loanApproved, setLoanApproved] = useState(true);
+    let navigate = useNavigate();
+    const [loanApproved, setLoanApproved] = useState(true);
 
-  const approveLoan = () => {
-    // Simulate loan approval
-    setLoanApproved(true);
-  };
+    const approveLoan = () => {
+        // Simulate loan approval
+        setLoanApproved(true);
+    };
 
-  const closeSuccessMessage = () => {
-    setLoanApproved(false);
-    navigate("/");
-  };
+    const closeSuccessMessage = () => {
+        setLoanApproved(false);
+        navigate("/");
+    };
 
-  return (
-    <div>
-      <button onClick={approveLoan}>Approve Loan</button>
+    return (
+        <div>
+            <button onClick={approveLoan}>Approve Loan</button>
 
-      {loanApproved && <SuccessMessage onClose={closeSuccessMessage} />}
-    </div>
-  );
+            {loanApproved && <SuccessMessage onClose={closeSuccessMessage} />}
+        </div>
+    );
 };
 
 export default LoanApplication;
